d73a51a89b0a39336bcf04432cc204da579d3e3c
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-23 11:42:33 +0200

    Add basic utils

---
+++ utils/api.js
@@ -0,0 +1,38 @@
+import {AsyncStorage} from 'react-native'
+import {DECKS_STORAGE_KEY, handleDecks} from './_decks'
+
+
+export function getDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then(handleDecks)
+}
+
+export function getDeck(id) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      const deck = data[id]
+      return deck
+    })
+}
+
+export function saveDeckTitle(title) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+    [title]: {
+      title: title,
+      questions: []
+    }
+  }))
+}
+
+export function addCardToDeck({title, card}) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      const deck = data[title]
+      deck.questions.push(card)
+      AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [title]: deck
+      }))
+    })
+}
