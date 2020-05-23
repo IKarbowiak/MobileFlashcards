864157cbf02908d66dcbd2e90fa02f3e6b3e21ee
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-23 22:53:00 +0200

    Add DeckView component

---
+++ components/DeckView.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+
+import {white} from '../utils/colors'
+
+
+class DeckView extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Test Deck</Text>
+        <TouchableOpacity
+          style={[styles.btnCont, {backgroundColor: white, borderWidth: 1, borderColor: 'black'}]}
+          onPress={this.submit}
+        >
+          <Text style={[styles.btnText, {color: 'black'}]}>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btnCont, {backgroundColor: 'black', color: white}]}
+          onPress={this.submit}
+        >
+          <Text style={styles.btnText}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    justifyContent: 'center',
+    padding: 40,
+  },
+  text: {
+    fontSize: 40,
+    textAlign: 'center',
+    paddingBottom: 20,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: Platform.OS === 'ios' ? 8 : 2,
+    padding: 5,
+  },
+  btnCont: {
+    padding: 10,
+    borderRadius: 10,
+    marginHorizontal: 100,
+    marginVertical: 10,
+  },
+  btnText: {
+    fontSize: 20,
+    color: white,
+    textAlign: 'center'
+  }
+})
+
+export default DeckView
